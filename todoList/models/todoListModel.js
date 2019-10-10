39334ea7500caf641649e329ce40221830e4085b
Israel Teneda <israteneda@gmail.com>
2019-10-09 23:28:33 -0500

    Cambio postgreSQL por SQLite

---
+++ todoList/models/todoListModel.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var Sequelize = require('sequelize');
+
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: '../todo_list.sqlite3',
+  define: {
+    timestamps: true
+  }
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const Task = sequelize.define('task', {
+  // attributes
+  ID: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  task: {
+    type: Sequelize.STRING,
+    allowNull:false,
+  },
+  status: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1,
+    allowNull:false
+  }
+}, {
+  // options
+});
+
+Task.create({ 
+    task: 'Buscar Bugs',
+    status: 1, 
+  }).then(task => {
+  console.log("Esta tarea a generado el ID:", task.id);
+})
+.then(function(task) {
+    // you can now access the newly created user
+    console.log('Creación exitosa');
+})
+.catch(function(err) {
+    // print the error details
+    console.log(err);
+});
+
+Task.findAll().then(tasks => {
+  console.log("Todas las tareas:", JSON.stringify(tasks, null, 4));
+});
